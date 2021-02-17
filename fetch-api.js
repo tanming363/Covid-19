de700255a5bf38f0d08d5ac54a330c7d597cfa96
Tanmay Ingole <tanmay.ingole@engi.pl>
2021-02-17 15:56:19 +0530

    added popup in map

--- fetch-api.js
+++ fetch-api.js
@@ -237,19 +237,19 @@ function initView() {
                                                 <div class="row mb-4">
                                                         <div class="col-3 pr-1">
                                                                 <small class="card-text">Cases</small>
-                                                                <h4>${data.cases}</h4>
+                                                                <h4>${numberWithWord(data.cases)}</h4>
                                                         </div>
                                                         <div class="col-3 pr-1 border-left">
                                                                 <small class="card-text">Recovered</small>
-                                                                <h4>${data.recovered}</h4>
+                                                                <h4>${numberWithWord(data.recovered)}</h4>
                                                         </div>
                                                         <div class="col-3 pr-1 border-left">
                                                                 <small class="card-text">Critical</small>
-                                                                <h4>${data.critical}</h4>
+                                                                <h4>${numberWithWord(data.critical)}</h4>
                                                         </div>
                                                         <div class="col-3 pr-1 border-left">
                                                                 <small class="card-text">Deaths</small>
-                                                                <h4>${data.deaths}</h4>
+                                                                <h4>${numberWithWord(data.deaths)}</h4>
                                                         </div>
                                                 </div>
                                         </div>
@@ -258,25 +258,10 @@ function initView() {
                         <div class="col-md-7 col-sm-6">
                                 <div class="card my-3">
                                         <div class="card-header bg-white py-3 align-middle">
-                                                Graph presentation
+                                                Map presentation
                                         </div>
-                                        <div class="card-body">
-                                                <div class="row">
-                                                        <div class="col-auto">
-                                                                <h5 class="card-title mb-3">
-                                                                        <svg class="mb-1 text-primary"
-                                                                                xmlns="http://www.w3.org/2000/svg" width="20"
-                                                                                height="20" fill="currentColor" class="bi bi-globe2"
-                                                                                viewBox="0 0 16 16">
-                                                                                <path
-                                                                                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855-.143.268-.276.56-.395.872.705.157 1.472.257
-                                                                        2.282.287V1.077zM4.249 3.539c.142-.384.304-.744.481-1.078a6.7 6.7 0 0 1 .597-.933A7.01 7.01 0 0 0 3.051 3.05c.362.184.763.349 1.198.49zM3.509 7.5c.036-1.07.188-2.087.436-3.008a9.124 9.124 0 0 1-1.565-.667A6.964 6.964 0 0 0 1.018 7.5h2.49zm1.4-2.741a12.344 12.344 0 0 0-.4 2.741H7.5V5.091c-.91-.03-1.783-.145-2.591-.332zM8.5 5.09V7.5h2.99a12.342 12.342 0 0 0-.399-2.741c-.808.187-1.681.301-2.591.332zM4.51 8.5c.035.987.176 1.914.399 2.741A13.612 13.612 0 0 1 7.5 10.91V8.5H4.51zm3.99 0v2.409c.91.03 1.783.145 2.591.332.223-.827.364-1.754.4-2.741H8.5zm-3.282 3.696c.12.312.252.604.395.872.552 1.035 1.218 1.65 1.887 1.855V11.91c-.81.03-1.577.13-2.282.287zm.11 2.276a6.696 6.696 0 0 1-.598-.933 8.853 8.853 0 0 1-.481-1.079 8.38 8.38 0 0 0-1.198.49 7.01 7.01 0 0 0 2.276 1.522zm-1.383-2.964A13.36 13.36 0 0 1 3.508 8.5h-2.49a6.963 6.963 0 0 0 1.362 3.675c.47-.258.995-.482 1.565-.667zm6.728 2.964a7.009 7.009 0 0 0 2.275-1.521 8.376 8.376 0 0 0-1.197-.49 8.853 8.853 0 0 1-.481 1.078 6.688 6.688 0 0 1-.597.933zM8.5 11.909v3.014c.67-.204 1.335-.82 1.887-1.855.143-.268.276-.56.395-.872A12.63 12.63 0 0 0 8.5 11.91zm3.555-.401c.57.185 1.095.409 1.565.667A6.963 6.963 0 0 0 14.982 8.5h-2.49a13.36 13.36 0 0 1-.437 3.008zM14.982 7.5a6.963 6.963 0 0 0-1.362-3.675c-.47.258-.995.482-1.565.667.248.92.4 1.938.437 3.008h2.49zM11.27 2.461c.177.334.339.694.482 1.078a8.368 8.368 0 0 0 1.196-.49 7.01 7.01 0 0 0-2.275-1.52c.218.283.418.597.597.932zm-.488 1.343a7.765 7.765 0 0 0-.395-.872C9.835 1.897 9.17 1.282 8.5 1.077V4.09c.81-.03 1.577-.13 2.282-.287z" />
-                                                                        </svg>
-                                                                        Worldwide
-                                                                </h5>
-                                                        </div>
-                                                </div>
-                                                       <div id="mapid"></div>
+                                        <div class="card-body p-0">
+                                               <div id="mapid"></div>
                                         </div>
                                 </div>
                         </div>
@@ -301,34 +286,65 @@ function initViewChart() {
                         var circle = [];
                         for (let i = 0; i < data.length; i++) {
                                 let el = data[i];
-
+                                // console.log(el);
                                 circle.push({
                                         cases: el.cases,
+                                        recovered: el.recovered,
+                                        deaths: el.deaths,
+                                        critical: el.critical,
+                                        country: el.country,
                                         lat: el.countryInfo.lat,
-                                        long: el.countryInfo.long
+                                        long: el.countryInfo.long,
+                                        img: el.countryInfo.flag
                                 });
+
                         }
 
                         var mymap = L.map('mapid').setView([10.45, 76.41], 2); //setView([lat, lon], 13)
 
                         L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
                                 id: 'mapbox/streets-v11',
-                                minZoom: 1,
+                                minZoom: 2,
                                 zoom: 15,
                         }).addTo(mymap);
 
                         for (let i = 0; i < circle.length; i++) {
                                 let el = circle[i];
-                                L.circle([el.lat, el.long], 900, {
+                                L.circle([el.lat, el.long], 500, {
                                         color: '#de3700a6',
                                         fillColor: '#de3700a6',
                                         fillOpacity: 0.5,
-                                }).addTo(mymap).bindPopup("Hello");
-                                // console.log(el.cases);
+                                }).addTo(mymap).bindPopup(
+                                        `<img class="border country-thumbnail mb-1 d-inline" width="30" src="${el.img}">
+                                        <h6 class="my-1 d-inline font-weight-bold">${el.country}</h6><br>                             
+                                        Cases: ${numberWithWord(el.cases)}<br>
+                                        Recovered: ${numberWithWord(el.recovered)}<br>
+                                        Critical: ${numberWithWord(el.critical)}<br>
+                                        Deaths: ${numberWithWord(el.deaths)}
+                                        `);
+                                mymap.on('zoomend', function (e) {
+                                        var newRadius = Math.pow(mymap.getZoom(), 20);
+                                        circle.setRadius(newRadius);
+                                });
                         }
                 })
                 .catch(err => {
                         console.log(err);
                 })
 
+}
+
+//convert number into M B K
+function numberWithWord(labelValue) {
+        const sign = Math.sign(Number(labelValue));
+        // Nine Zeroes for Billions
+        return Math.abs(Number(labelValue)) >= 1.0e9
+                ? sign * (Math.abs(Number(labelValue)) / 1.0e9).toFixed(2) + "B"
+                : // Six Zeroes for Millions
+                Math.abs(Number(labelValue)) >= 1.0e6
+                        ? sign * (Math.abs(Number(labelValue)) / 1.0e6).toFixed(2) + "M"
+                        : // Three Zeroes for Thousands
+                        Math.abs(Number(labelValue)) >= 1.0e3
+                                ? sign * (Math.abs(Number(labelValue)) / 1.0e3).toFixed(2) + "K"
+                                : Math.abs(Number(labelValue));
 }
